69f1e8cfc5868c0f357f37acfbd75156411eca2a
Nicolas <blanc-nicolas@outlook.com>
2024-11-13 15:58:56 +0100

    front SOLUCE

--- components/Home.js
+++ components/Home.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import { Popover, Button } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import Movie from './Movie';
-import 'antd/dist/antd.css';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from "react";
+import { Popover, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import Movie from "./Movie";
+import "antd/dist/antd.css";
+import styles from "../styles/Home.module.css";
 
 function Home() {
   const [likedMovies, setLikedMovies] = useState([]);
-  const [moviesList, setMoviesList] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
 
   // Liked movies (inverse data flow)
   const updateLikedMovies = (movieTitle) => {
-    if (likedMovies.find(movie => movie === movieTitle)) {
-      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    if (likedMovies.find((movie) => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter((movie) => movie !== movieTitle));
     } else {
       setLikedMovies([...likedMovies, movieTitle]);
     }
@@ -23,30 +23,58 @@ function Home() {
     return (
       <div key={i} className={styles.likedMoviesContainer}>
         <span className="likedMovie">{data}</span>
-        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          onClick={() => updateLikedMovies(data)}
+          className={styles.crossIcon}
+        />
       </div>
     );
   });
 
   const popoverContent = (
-    <div className={styles.popoverContent}>
-      {likedMoviesPopover}
-    </div>
+    <div className={styles.popoverContent}>{likedMoviesPopover}</div>
   );
 
   // Movies list
   useEffect(() => {
-    fetch('https://mymoviz-back-inky.vercel.app/movies')
-      .then(response => response.json())
-      .then(data => {
-        setMoviesList(data.movies);
-      });
-  }, [])
+    fetch("https://mymoviz-back-inky.vercel.app/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const formatedData = data.movies.map((movie) => {
+          const poster = https://image.tmdb.org/t/p/w500/${movie.poster_path};
+          let overview = movie.overview;
+          if (overview.length > 250) {
+            overview = overview.substring(0, 250) + "...";
+          }
 
+      return {
+        title: movie.title,
+        poster,
+        voteAverage: movie.vote_average,
+        voteCount: movie.vote_count,
+        overview,
+      };
+    });
+    setMoviesData(formatedData);
+  });
+  }, []);
 
-  const movies = moviesList.map((data, i) => {
-    const isLiked = likedMovies.some(movie => movie === data.original_title);
-    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.original_title} overview={data.overview} poster={`https://image.tmdb.org/t/p/w500${data.poster_path}`} voteAverage={data.vote_average} voteCount={data.vote_count} />;
+  const movies = moviesData.map((data, i) => {
+    const isLiked = likedMovies.some((movie) => movie === data.title);
+    return (
+      <Movie
+        key={i}
+        updateLikedMovies={updateLikedMovies}
+        isLiked={isLiked}
+        title={data.title}
+        overview={data.overview}
+        poster={data.poster}
+        voteAverage={data.voteAverage}
+        voteCount={data.voteCount}
+      />
+    );
   });
 
   return (
@@ -56,16 +84,20 @@ function Home() {
           <img src="logo.png" alt="Logo" />
           <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
         </div>
-        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+        <Popover
+          title="Liked movies"
+          content={popoverContent}
+          className={styles.popover}
+          trigger="click"
+        >
           <Button>♥ {likedMovies.length} movie(s)</Button>
         </Popover>
       </div>
       <div className={styles.title}>LAST RELEASES</div>
-      <div className={styles.moviesContainer}>
-        {movies}
-      </div>
+      <div className={styles.moviesContainer}>{movies}</div>
     </div>
   );
 }
 
 export default Home;
+
